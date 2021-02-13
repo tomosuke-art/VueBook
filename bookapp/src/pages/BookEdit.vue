7fa0ad7e2840f60544ccf8037feafab62dfe02c4
tomosuke-art <tomo1130taro@icloud.com>
2021-02-13 17:15:50 +0900

    DOM更新を待つ処理()

--- bookapp/src/pages/BookEdit.vue
+++ bookapp/src/pages/BookEdit.vue
@@ -1,5 +1,6 @@
 <template>
   <div>BookEdit
+    {{ books.title}}
     {{ books }}
   </div>
 </template>
@@ -9,6 +10,23 @@ export default {
    name:'BookEdit',
    props:{
      books:Array
+   },
+   // propsで配列で入ってくるのでbookに一つの情報を入れれるようにする
+   data(){
+     return{
+       book:'' // オブジェクト
+     }
+   },
+   // thisのアクセスはできない
+   // 代わりにvmでインスタンスにアクセス 
+   beforeRouteEnter (to, from, next) {
+    next(vm => {
+      // DOM更新がされた後に更新するように
+      vm.$nextTick(()=>{
+        vm.book = vm.books[vm.$route.params.id]
+        console.log(vm.book)
+      })
+    })
    }
 }
 </script>
